0ab1f6502b41a25a2dca8bf90ba0873cbd90e7be
gzstyp <444141300@qq.com>
2020-05-06 16:58:15 +0800

    改名

--- jdbc/src/main/java/com/fwtai/Jdbc.java
+++ jdbc/src/main/java/com/fwtai/Jdbc.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Server{
+public class Jdbc{
 
     public static void main(String[] args){
-        SpringApplication.run(Server.class,args);
+        SpringApplication.run(Jdbc.class,args);
     }
 }
--- memory/src/main/java/com/fwtai/Memory.java
+++ memory/src/main/java/com/fwtai/Memory.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Server{
+public class Memory{
 
     public static void main(String[] args){
-        SpringApplication.run(Server.class,args);
+        SpringApplication.run(Memory.class,args);
     }
 }
